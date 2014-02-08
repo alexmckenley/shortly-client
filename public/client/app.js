cc4da21081c4bbf83a0ff806c7c15913e33f645a
Hack Reactor Students <alexmckenley@gmail.com>
2014-02-08 10:30:11 -0800

    implemented LinkService instead of regular http calls

--- public/client/app.js
+++ public/client/app.js
@@ -12,16 +12,44 @@ var app = angular.module("shortlyApp", ['ngRoute'])
   });
 
 })
-.controller('linksController', function($scope, $http){
+//User Service
+.service('UserService', function(){
+  var currentUser = null;
+  this.setUser = function(u){
+    currentUser = u;
+  };
+  this.currentUser = function(){
+    return currentUser;
+  };
+})
+//Link Service
+.service("LinkService", function($http){
+  this.getLinks = function(){
+    return $http({
+      method: 'GET',
+      url: '/links'
+    });
+  };
+
+  this.createLink = function(url) {
+    return $http({
+      method: 'POST',
+      url: '/links',
+      data: JSON.stringify({url: url})
+    });
+  };
+})
+//Authentication Service
+.service('AuthService', function($http, UserService){
+
+})
+.controller('linksController', function($scope, LinkService){
   $scope.sortBy = 'visits';
   $scope.reverse = true;
   $scope.cats = ['title','visits'];
 
 
-  $http({
-    method: 'GET',
-    url: '/links'
-  }).success(function(data, statusCode){
+  LinkService.getLinks().success(function(data, statusCode){
     $scope.links = data;
 
     console.log("Success", data);
@@ -31,15 +59,11 @@ var app = angular.module("shortlyApp", ['ngRoute'])
   });
 
 })
-.controller('createController', function($scope, $http){
+.controller('createController', function($scope, LinkService){
   $scope.added = [];
 
   $scope.createLink = function(){
-    $http({
-      method: 'POST',
-      url: '/links',
-      data: JSON.stringify($scope.newLink)
-    }).success(function(data, statusCode){
+    LinkService.createLink($scope.newLink.url).success(function(data, statusCode){
       console.log("Created SUccessfully: ", data);
       $scope.added.push(data);
     }).error(function(err){
