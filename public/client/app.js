c60668db11939a3b628f58504940bd28f676c8f6
Hack Reactor Students <alexmckenley@gmail.com>
2014-02-08 15:50:47 -0800

    added loginView

--- public/client/app.js
+++ public/client/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("shortlyApp", ['ngRoute'])
 .config(function($routeProvider, $httpProvider){
-  $httpProvider.interceptors.push('HttpAuthInteceptor');
+  $httpProvider.interceptors.push('AuthService');
 
   $routeProvider
   .when('/', {
@@ -10,9 +10,14 @@ var app = angular.module("shortlyApp", ['ngRoute'])
   .when('/create', {
     controller: 'createController',
     templateUrl: 'client/views/createView.html'
+  })
+  .when('/login', {
+    controller: 'loginController',
+    templateUrl: 'client/views/loginView.html'
   });
 
 })
+
 //User Service
 .service('UserService', function(){
   var currentUser = null;
@@ -20,7 +25,11 @@ var app = angular.module("shortlyApp", ['ngRoute'])
     currentUser = u;
   };
   this.currentUser = function(){
-    return true;
+    return currentUser;
+  };
+
+  this.login = function(u){
+
   };
 })
 //Link Service
@@ -40,8 +49,9 @@ var app = angular.module("shortlyApp", ['ngRoute'])
     });
   };
 })
+
 //Authentication Service
-.service('HttpAuthInteceptor', function(UserService){
+.service('AuthService', function(UserService, $location){
   var encodeData = function (data) {
      var ret = [];
      for (var d in data)
@@ -53,21 +63,37 @@ var app = angular.module("shortlyApp", ['ngRoute'])
     if(UserService.currentUser()){
       req.url += "?" + encodeData({token: Math.random()}); 
     }
-    //console.log("Request: ", req.method, req.url, req.data);
     return req;
   };
   this.response = function(res){
-    console.dir(res);
+
     return res;
   };
   this.requestError = function(req){
     return req;
   };
   this.responseError = function(res){
+    if (res.status == 401) {
+      console.dir("REJECTED");
+      $location.path("/login");
+    }
     return res;
   };
 
+  this.login = function(){
+    $http({
+      method: 'POST',
+      url: '/login',
+      data: u
+    }).success(function(data){
+      currentUser = data;
+      $location.path('/');
+    }).error(function (err) {
+      console.log("error logging in: ", err);
+    });
+  };
 })
+
 .controller('linksController', function($scope, LinkService){
   $scope.sortBy = 'visits';
   $scope.reverse = true;
@@ -84,6 +110,7 @@ var app = angular.module("shortlyApp", ['ngRoute'])
   });
 
 })
+
 .controller('createController', function($scope, LinkService){
   $scope.added = [];
 
@@ -95,5 +122,13 @@ var app = angular.module("shortlyApp", ['ngRoute'])
       console.log("There was an error!", err);
     });
   };
+
+})
+
+.controller('loginController', function($scope, UserService){
+
+  $scope.login = function () {
+    
+  }
 
 });
